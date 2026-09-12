d5c0f848ab9a8712a9fe2712df93d43ca1667025
zafrankhanit-dev <zafrankhanit@gmail.com>
2026-09-12 20:04:52 +0500

    Update Logo.jsx

--- thevcc-smart-test/src/components/Logo.jsx
+++ thevcc-smart-test/src/components/Logo.jsx
@@ -9,7 +9,7 @@ export default function Logo({ size = 'md', className = '' }) {
 
   return (
     <img
-      src="/final logo 02.png"
+      src="/logo.png"
       alt="The Vision Coaching Centre"
       className={`${dims} object-contain ${className}`}
     />
