35bc37a7a82ace82d2175212ab9708f3f66795a5
zafrankhanit-dev <zafrankhanit@gmail.com>
2026-09-13 08:58:24 +0500

    Update Logo.jsx

--- thevcc-smart-test/src/components/Logo.jsx
+++ thevcc-smart-test/src/components/Logo.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 export default function Logo({ size = 'md', className = '' }) {
   const dims = {
-    sm: 'h-9 w-9',
-    md: 'h-12 w-12',
-    lg: 'h-16 w-16',
+    sm: 'h-12 w-12',
+    md: 'h-20 w-20',
+    lg: 'h-28 w-28',
   }[size];
 
   return (
